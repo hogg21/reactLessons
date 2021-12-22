a8ff04d9970ed2dce74534b1021782e704877ed7
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-23 01:11:15 +0200

    Add files via upload

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// should render following html
-// <h1 class="title">Todo List</h1>
-
 
 
 ReactDOM.render(
-  <h1 className='title'>Todo List</h1>,
+  <h1 class='title'>Todo List</h1>,
   document.getElementById('root')
 )
