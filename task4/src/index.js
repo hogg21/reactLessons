fc137114c7fffd869f754cc510e7eaecafcd0af3
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-22 23:32:16 +0200

    Add files via upload

---
+++ task4/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+    <div class="greeting">
+        <h1 class="greeting__title">Hello, world!</h1>
+        <p class="greeting__text">I'm learning React</p>
+    </div>
+)
+console.log(element);
+
+ReactDOM.render(element, rootElement)
