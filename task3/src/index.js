2a44e0a1ba4dbf2d3718d1f7b965cb9ae6a703b0
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-23 00:27:29 +0200

    Add files via upload

--- task3/src/index.js
+++ task3/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 import './styles.css';
-
 const rootElement = document.querySelector('#root');
 
-const element = (
-    <div class="greeting">
-        <h1 class="greeting__title">Hello, world!</h1>
-        <p class="greeting__text">I'm learning React</p>
-    </div>
+ReactDOM.render(
+  <div class="greeting">
+    <h1 class="greeting__title">Hello, world!</h1>
+    <p class="greeting__text">I'm learning React</p>
+  </div>,
+  rootElement
 )
-console.log(element);
-
-ReactDOM.render(element, rootElement)
