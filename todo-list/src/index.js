8b274c3ce5af79eb1f8697e12f2fd5f2ba69be3b
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-23 10:16:06 +0200

    Delete todo-list directory

--- todo-list/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-
-ReactDOM.render(
-  <h1 className='title'>Todo List</h1>,
-  document.getElementById('root')
-)
