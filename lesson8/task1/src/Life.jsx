5c717aebfda9996dbae4a5a6843ce2088bf10d69
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-29 20:15:16 +0200

    Add files via upload

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -3,22 +3,22 @@ import React, { Component } from "react";
 class Life extends Component {
     constructor(props) {
         super(props);
-        console.log('good place to create state');
+        console.log('constructor: good place to create state');
     }
     componentDidMount() {
-        console.log('API calls, subscriptions');
+        console.log('componentDidMount: API calls, subscriptions');
     }
     shouldComponentUpdate(nextProps, nextState) {
         console.log(nextProps, nextState);
-        console.log('decide to render ot not to render');
+        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
         return nextProps.number % 2;
     }
     componentDidUpdate(prevProps, prevState) {
         console.log(prevProps);
-        console.log('some updates based on new props')
+        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
     }
     componentWillUnmount() {
-        console.log('cleanup before DOM related to component will be removed')
+        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed')
     }
     render() {
         console.log('return React element to build DOM');
