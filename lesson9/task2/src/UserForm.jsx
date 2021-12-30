bac556c6410bd043a184a296e6f2af4476d6e516
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-30 19:01:54 +0200

    Add files via upload

--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -22,7 +22,7 @@ class UserForm extends Component {
     }   
     render() {
         return (
-            <form className="login-form" onSubmit={() => this.handleSubmit(this.state)}>
+            <form className="login-form" onSubmit={() => this.props.onSubmit(this.state)}>
                 <h1 className="form-title">Profile</h1>
                 <div className="form-control">
                     <label className="form-label" htmlFor="name">Name</label>
