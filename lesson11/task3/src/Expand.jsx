02a3165cf0f44a9dd1ca754f2ca42848c4124db9
hogg21 <89107241+hogg21@users.noreply.github.com>
2022-01-03 02:06:57 +0200

    Add files via upload

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -15,14 +15,14 @@ class Expand extends Component {
                 <div className="expand__header">
                     <span className="expand__title">{this.props.title}</span>
                     <button className="expand__toggle-btn" onClick={this.toggleShow}>
-                        {this.state.isVisible ? (
+                        {this.state.isShow ? (
                             <i className="fas fa-chevron-up"></i>
                         ) : (
                             <i className="fas fa-chevron-down"></i>
                         )}
                     </button>
                 </div>
-                {this.state.isVisible ? <div className="expand__content">{this.props.children}</div> : null}
+                {this.state.isShow ? <div className="expand__content">{this.props.children}</div> : null}
             </div>
         );
     }
