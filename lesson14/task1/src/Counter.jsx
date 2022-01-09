e75ec328391117b22d3b3122662484acdb3eb11b
hogg21 <89107241+hogg21@users.noreply.github.com>
2022-01-09 21:57:41 +0200

    Add files via upload

--- lesson14/task1/src/Counter.jsx
+++ lesson14/task1/src/Counter.jsx
@@ -2,13 +2,13 @@ import React, {useState} from "react";
 
 
 const Counter = () => {
-    const [counter, setCounter] = useState(17);
+    const [counter, setCounter] = useState(0);
     return (
         <div className="counter">
             <button className="counter__button" onClick={() => setCounter(counter - 1)}>
                 -
             </button>
-            <span className="counter__value" onClick={() => setCounter(17)}>
+            <span className="counter__value" onClick={() => setCounter(0)}>
                 {counter}
             </span>
             <button className="counter__button" onClick={() => setCounter(counter + 1)}>
